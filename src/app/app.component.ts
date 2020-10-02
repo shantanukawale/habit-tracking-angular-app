2e033ba46b7d600a24e462a7695ad93f73d9013d
Shantanu Kawale <shantanu.kawale@whitehatjr.com>
2020-10-03 02:06:43 +0530

    add list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Habit } from './models/habit';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'habit-tracking-angular-app';
+  public habits: Habit[] = [
+    <Habit>{
+      name: '15 Minute Walk',
+      frequency: 'Daily',
+      description:
+        'This habit makes my kitchen look nice and makes my day better the next morning.',
+    },
+    <Habit>{
+      name: 'Weed the Garden',
+      frequency: 'Weekly',
+      description:
+        'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
+    },
+  ];
 }
